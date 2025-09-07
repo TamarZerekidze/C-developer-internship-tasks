8bc35a816009a07e8f741933c0c584f23b58c792
TamarZerekidze <zerekidzetamar03@gmail.com>
2025-09-07 23:42:27 +0400

    using dfs approach, added recursive logic of tracing subdirectories downwards

--- task_2_solution/main.c
+++ task_2_solution/main.c
@@ -1,16 +1,34 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <string.h>
 
 int isHidden(const struct dirent* entry) {
 	return entry->d_name[0] == '.';
 }
 
-void printDIRContents(DIR *dir) {
+void printDIRContents(DIR *dir, const char* current_path) {
 	struct dirent* entry;
 	while ((entry = readdir(dir))) {
 		if (isHidden(entry)) continue;
 		printf("%s\n", entry->d_name);
+
+		if (entry->d_type == DT_DIR) {
+			char new_path[PATH_MAX];
+			strcpy(new_path, current_path);
+			strcat(new_path, "/");
+			strcat(new_path, entry->d_name);
+
+			DIR *newDir;
+			if ((newDir = opendir(new_path))) {
+				printDIRContents(newDir, new_path);
+				if (closedir(newDir)) {
+					perror("could not close directory");
+				}
+			} else {
+				perror("could not open directory");
+			}
+		}
 	}
 }
 
@@ -24,7 +42,7 @@ int main(void) {
 
 	DIR *cwdDir;
 	if ((cwdDir = opendir(cwd))) {
-		printDIRContents(cwdDir);
+		printDIRContents(cwdDir, cwd);
 		if (closedir(cwdDir)) {
 			perror("could not close directory");
 			return 1;
