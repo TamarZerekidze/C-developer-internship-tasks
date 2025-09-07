76476c67a953711e17e1d9a34bbbaa9fa6794522
TamarZerekidze <zerekidzetamar03@gmail.com>
2025-09-07 23:34:27 +0400

    wrote printing first layer of directory using readdir() function

--- task_2_solution/main.c
+++ task_2_solution/main.c
@@ -2,6 +2,13 @@
 #include <dirent.h>
 #include <stdio.h>
 
+void printDIRContents(DIR *dir) {
+	struct dirent* entry;
+	while ((entry = readdir(dir))) {
+		printf("%s\n", entry->d_name);
+	}
+}
+
 int main(void) {
 	char cwd[PATH_MAX];
 	if (getcwd(cwd, sizeof(cwd)) == NULL) {
@@ -9,5 +16,18 @@ int main(void) {
 		return 1;
 	}
 	printf("%s\n", cwd);
+
+	DIR *cwdDir;
+	if ((cwdDir = opendir(cwd))) {
+		printDIRContents(cwdDir);
+		if (closedir(cwdDir)) {
+			perror("could not close directory");
+			return 1;
+		}
+	} else {
+		perror("could not open directory");
+		return 1;
+	}
+
 	return 0;
 }
