2ece0b2bc1111ee265d382710bc451c23a31f22f
TamarZerekidze <zerekidzetamar03@gmail.com>
2025-09-07 23:48:01 +0400

    decided to decompose duplicated code

--- task_2_solution/main.c
+++ task_2_solution/main.c
@@ -1,10 +1,29 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+void printDIRContents(DIR *dir, const char* current_path);
 
 int isHidden(const struct dirent* entry) {
 	return entry->d_name[0] == '.';
+}
+
+void visitDIR(const char *dir_path) {
+	DIR *dir;
+
+	if ((dir = opendir(dir_path))) {
+		printDIRContents(dir, dir_path);
+
+		if (closedir(dir)) {
+			perror("could not close directory");
+			exit(1);
+		}
+	} else {
+		perror("could not open directory");
+		exit(1);
+	}
 }
 
 void printDIRContents(DIR *dir, const char* current_path) {
@@ -19,15 +38,7 @@ void printDIRContents(DIR *dir, const char* current_path) {
 			strcat(new_path, "/");
 			strcat(new_path, entry->d_name);
 
-			DIR *newDir;
-			if ((newDir = opendir(new_path))) {
-				printDIRContents(newDir, new_path);
-				if (closedir(newDir)) {
-					perror("could not close directory");
-				}
-			} else {
-				perror("could not open directory");
-			}
+			visitDIR(new_path);
 		}
 	}
 }
@@ -36,21 +47,11 @@ int main(void) {
 	char cwd[PATH_MAX];
 	if (getcwd(cwd, sizeof(cwd)) == NULL) {
 		perror("could not get cwd\n");
-		return 1;
+		exit(1);
 	}
 	printf("%s\n", cwd);
 
-	DIR *cwdDir;
-	if ((cwdDir = opendir(cwd))) {
-		printDIRContents(cwdDir, cwd);
-		if (closedir(cwdDir)) {
-			perror("could not close directory");
-			return 1;
-		}
-	} else {
-		perror("could not open directory");
-		return 1;
-	}
+	visitDIR(cwd);
 
 	return 0;
 }
