615189cfa11042258ec230b6eb9b95ed4ebcd576
TamarZerekidze <zerekidzetamar03@gmail.com>
2025-09-09 01:33:45 +0400

    extended c implementation for prog1 && prog2 | prog3. output of last program is in terminal.

--- task_3_solution/main.c
+++ task_3_solution/main.c
@@ -4,12 +4,13 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	if (argc != 3) {
+	if (argc != 4) {
 		exit(1);
 	}
 
 	const char *prog1 = argv[1];
 	const char *prog2 = argv[2];
+	const char *prog3 = argv[3];
 
 	if (fork() == 0) {
 		execl(prog1, prog1, NULL);
@@ -22,13 +23,26 @@ int main(int argc, char *argv[]) {
 	if (status1 != 0) {
 		exit(1);
 	}
-
+	int fds[2];
+	pipe(fds);
 	if (fork() == 0) {
+		close(fds[0]);
+		dup2(fds[1], 1);
+		close(fds[1]);
 		execl(prog2, prog2, NULL);
 		perror("execl prog2 failed");
 		exit(1);
 	}
-
+	if (fork() == 0) {
+		close(fds[1]);
+		dup2(fds[0], 0);
+		close(fds[0]);
+		execl(prog3, prog3, NULL);
+		perror("execl prog3 failed");
+		exit(1);
+	}
+	close(fds[0]);
+	close(fds[1]);
 	wait(NULL);
-	exit(0);
+	wait(NULL);
 }
